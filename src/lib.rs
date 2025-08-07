4db7feae531f90439b8f5ee492af01643402855d
唐加彬 <me@tangjiabin.cn>
2025-08-07 14:40:22 +0800

    导出ConnectionManager

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,4 @@ pub use connector::RedisConnector;
 pub use error::{ConnectionError, Result};
 pub use pool::{PoolConfig, RedisPool};
 pub use utils::RedisUtils;
+pub use redis::aio::ConnectionManager;
